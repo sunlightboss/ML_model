b210d841988f52080a8adb9b83ac5b9b6f4440ce
sunlightboss <pajcoko7@gmail.com>
2026-02-22 01:30:01 +0600

    huy

--- Data/water_data.py
+++ Data/water_data.py
@@ -2,3 +2,5 @@ import pandas as pd
 
 df = pd.read_csv('/home/nursss/Загрузки/Smart_irrigation_dataset.csv')
 print(df.head(10))
+df_half = df.sample(frac=0.5, random_state=42)  # случайная половина
+df_half = df_half.to_csv('half_water.csv', index=True)
